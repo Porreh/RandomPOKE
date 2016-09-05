8693ceaa1bd0f9596d6ef8b5e904f42c5f8a1871
Dmytro <Porreh@gmail.com>
2016-09-05 11:07:39 +0300

    Create mainES6.js

---
+++ mainES6.js
@@ -0,0 +1,26 @@
+const url = 'https://pokeapi.co/api/v2/pokemon/';
+let $pokemon = $('.pokemon');
+let $button = $('.button');
+let $sprite = $('.sprite img');
+let $name = $('.name');
+
+function getPokemon(id) {
+  $.ajax(`${url}${id}/`)
+    .done(response => renderPokemon(response));
+}
+
+function renderPokemon(pokemon) {
+  $sprite.attr('src', pokemon.sprites.front_default);
+  $name.html(pokemon.name);
+}
+
+$button.click(function () {
+  getPokemon(Math.floor(Math.random() * (721 - 1)) + 1);
+  if ($pokemon.hasClass('hidden')) {
+    $pokemon.removeClass('hidden');
+    $pokemon.css('opacity', '0');
+    setTimeout(function () {
+      $pokemon.css('opacity', '1');
+    }, 1000);
+  }
+});
